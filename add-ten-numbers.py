be2bcd29e6e4279c6da93f0a91ddf95e142c5da0
vickyogundipe <115558160+vickyogundipe@users.noreply.github.com>
2023-10-02 13:20:36 +0100

    Add files via upload

---
+++ add-ten-numbers.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+total = 0
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+n = int(input())
+total = (total + n)
+
+print(total)
+
+print(total)
---
+++ add-ten-odd-numbers.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+total = 0
+
+n = int(input())
+total = (n + total)
+
+n = int(input())
+total = (n + total)
+
+n = int(input())
+total = (n + total)
+
+n = int(input())
+total = (n + total)
+
+n = int(input())
+total = (n + total)
+
+n = int(input())
+total = (n + total)
+
+n = int(input())
+total = (n + total)
+
+n = int(input())
+total = (n + total)
+
+n = int(input())
+total = (n + total)
+
+n = int(input())
+total = (n + total)
+
+print(total)
